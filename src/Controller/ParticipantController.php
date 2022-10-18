67f6ad1060c99d619b67b3b8f5e7edb03dc66324
fhure2021 <fabrice.hure.35@gmail.com>
2022-10-18 11:55:36 +0200

    Création d'un formulaire monProfil

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -2,9 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\Participant;
+use App\Entity\User;
+use App\Form\ProfilType;
+use App\Form\RegistrationFormType;
+use App\Security\AuthentificationAuthenticator;
+use App\Security\UserAuthenticator;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
 class ParticipantController extends AbstractController
 {
@@ -13,6 +23,17 @@ class ParticipantController extends AbstractController
     {
         return $this->render('participant/index.html.twig', [
             'controller_name' => 'ParticipantController',
+        ]);
+    }
+
+    #[Route('/monprofil', name: 'app_monprofil')]
+    public function monProfil(): Response
+    {
+        $user = $this ->getUser();
+        $form = $this->createForm(ProfilType::class, $user);
+
+        return $this->render('participant/monProfil.html.twig', [
+            'monProfilForm' => $form->createView(),
         ]);
     }
 }
