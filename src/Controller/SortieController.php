473cfe7cf0ab2e3a90d03073d7817d3cd221e48e
Tsekko <maxime.kervadec@laposte.net>
2022-10-19 16:29:45 +0200

    Fix erreur sur un conflit

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -10,6 +10,7 @@ use App\Repository\SortieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
